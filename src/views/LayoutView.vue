a66c9c867acba7e56be3f82a98b961cf56c43e7f
Vaseem Akram <505073150@penske.com>
2026-06-21 16:32:36 +0530

    feat: integrate Quasar framework and update project structure
    - Added Quasar dependencies and configured Quasar in main.js
    - Replaced HelloWorld component with LayoutView for a new layout structure
    - Simplified TheWelcome component to a basic welcome message
    - Created LayoutView.vue for a responsive layout with a header and drawer
    - Updated package.json with new dependencies for Quasar and related plugins
    - Added quasar-variables.sass for custom theme variables
    - Adjusted vite.config.js to include Quasar plugin and asset URL transformations

---
+++ src/views/LayoutView.vue
@@ -0,0 +1,39 @@
+<template>
+  <q-layout view="hHh lpR fFf">
+    <q-header reveal elevated class="bg-primary text-white" height-hint="98">
+      <q-toolbar>
+        <q-btn dense flat round icon="menu" @click="toggleLeftDrawer" />
+
+        <q-toolbar-title> API Explorer </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer v-model="leftDrawerOpen" side="left" elevated>
+      <div>
+        <q-list>
+          <q-item clickable class="text-primary" v-ripple to="/">
+            <q-item-section>Home</q-item-section>
+          </q-item>
+
+          <q-item clickable class="text-primary" v-ripple to="/about">
+            <q-item-section>About</q-item-section>
+          </q-item>
+        </q-list>
+      </div>
+      <!-- drawer content -->
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const leftDrawerOpen = ref(false)
+const toggleLeftDrawer = () => {
+  leftDrawerOpen.value = !leftDrawerOpen.value
+}
+</script>
--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,25 @@
 import { fileURLToPath, URL } from 'node:url'
+import { join } from 'node:path'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
     vueJsx(),
     vueDevTools(),
+    vue({
+      template: { transformAssetUrls },
+    }),
+    // @quasar/plugin-vite options list:
+    // https://github.com/quasarframework/quasar/blob/dev/vite-plugin/index.d.ts
+    quasar({
+      sassVariables: join(import.meta.dirname, 'src/quasar-variables.sass'),
+    }),
   ],
   resolve: {
     alias: {
